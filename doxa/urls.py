77db356539f5519b296fd6753afee3b411605ba6
Harshul Jain <harshulj@gmail.com>
2013-01-29 21:21:03 +0530

    Added static routing for production.

--- doxa/urls.py
+++ doxa/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic.simple import direct_to_template
 
+
+from doxa import settings 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -15,5 +17,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', direct_to_template, { 'template': 'index.html'} ),
+    url(r'^$', direct_to_template, { 'template': 'index.html'} ),
 )
+
+if settings.DEBUG:
+	urlpatterns += patterns('',(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),)
