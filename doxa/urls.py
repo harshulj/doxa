e5c617e2e00c4ace26418888664d25eb257ed958
Harshul Jain <harshulj@gmail.com>
2013-01-30 10:38:53 +0530

    Added url to registration.

--- doxa/urls.py
+++ doxa/urls.py
@@ -17,6 +17,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    (r'^accounts/', include('registration.urls')),
     url(r'^$', direct_to_template, { 'template': 'index.html'} ),
 )
 
