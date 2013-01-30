edb2240fe79e785e430dbbd495959c6115f8ed9a
Harshul Jain <harshulj@gmail.com>
2013-01-30 12:37:34 +0530

    Corrected dj_database_url setting to accomodate local database setting.

--- doxa/settings.py
+++ doxa/settings.py
@@ -170,4 +170,4 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='mysql://doxa:doxa@localhost:3306/doxa')
