ba161abf4fd91df35e2333e2cd80853e2bc3f6d8
Harshul Jain <harshulj@gmail.com>
2013-02-01 20:27:19 +0530

    changed register url to new backend. account.reg.backend.doxa.url

--- doxa/urls.py
+++ doxa/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    (r'^accounts/', include('registration.urls')),
+    (r'^accounts/', include('account.reg.backends.doxa.urls')),
     url(r'^$', direct_to_template, { 'template': 'index.html'} ),
 )
 
