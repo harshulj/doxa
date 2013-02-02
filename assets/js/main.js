4eaac9267dc8bacef22459ee89323b17380b8684
Harshul Jain <harshulj@gmail.com>
2013-02-03 00:01:13 +0530

    Added base css and main js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
+$(document).ready(function(){
+	$(".dropdown-toggle").click(function(){
+		var x = $(this).attr("id");
+		if(x==1){
+			$(".submenu").hide();
+			$(this).attr("id", "0");
+		}
+		else{
+			$(".submenu").show();
+			$(this).attr("id", "1");
+		}
+		return false;
+	});
+	
+	// If mouse clicks on submenu
+	$(".dropdown-toggle").mouseup(function(){
+		return false
+	});
 
+	// If mouse is clicked on document
+	$(document).mouseup(function(){
+		$(".submenu").hide();
+		$(".dropdown-toggle").attr("id", "");
+	});
+});
