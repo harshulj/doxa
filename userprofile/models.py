bd5a901ba4d9755538f1cead1b91fd8fe0e92d91
Harshul Jain <harshulj@gmail.com>
2013-02-04 17:10:27 +0530

    Added user profile and profilepic models.

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
 
-# Create your models here.
+from userprofile.countries import CountryField
+
+GENDER_CHOICES = ( ('F', _('Female')), ('M', _('Male')),)
+
+class UserProfile(models.Model):
+	"""
+		This is a user profile model.
+	"""
+	user = models.ForeignKey(User, unique=True)
+	short_bio = models.CharField(max_length=100, null=True, blank=True)
+	dob = models.DateField(null=True, blank=True)
+	country = CountryField(null=True, blank=True)
+	gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+	url = models.URLField(max_length=70, null=True, blank=True)
+	about = models.TextField(max_length=200, null=True, blank=True)
+
+	def __unicode__(self):
+		return _("%s %s's profile") % self.user.first_name % self.user.last_name
+
+class ProfilePic(models.Model):
+	"""
+		Model for a users profile pic.
+	"""
+	image = models.ImageField(upload_to="profilepic/%Y/%b/%d")
+	user = models.ForeignKey(User, unique=True)
+	valid = models.BooleanField()
+
+	class Meta:
+		unique_together = (('user', 'valid'),)
+
+	def __unicode__(self):
+		return _("%s %s's Profile Pic") % self.user.first_name % self.user.last_name
+
+	def delete(self):
+		base, filename = os.path.split(self.image.path)
+		name, extension = os.path.splitext(filename)
+		for key in PROFILEPIC_SIZES:
+			try:
+				os.remove(os.path.join(base, "%s.%s%s" % (name, key, extension)))
+			except:
+				pass
+		super(ProfilePic, self).delete()
+
+	def save(self):
+		for pic in ProfilePic.objects.filter(user=self.user, valid=self.valid).exclude(id=self.id):
+			base, filename = os.path.split(pic.image.path)
+			name, extension = os.path.splitext(filename)
+			for key in PROFILEPIC_SIZES:
+				try:
+					os.remove(os.path.join(base, "%s.%s%s" % (name, key, extension)))
+				except:
+					pass
+			pic.delete()
+		super(ProfilePic, self).save()
