b9151fb65628020f79fed2e6f940bb00b9fcbe11
Harshul Jain <harshulj@gmail.com>
2013-02-05 20:29:51 +0530

    Added my_profile view.

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+
+from userprofile.models import UserProfile, ProfilePic
+
+@login_required
+def my_profile(request, template="userprofile/my_profile.html"):
+	profile = get_object_or_404(UserProfile, user=request.user)
+	return render_to_response(template, context_instance=RequestContext(request))
+
