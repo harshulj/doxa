71daef5b1e673a722d9b5c9a42eea022ed533434
Harshul Jain <harshulj@gmail.com>
2013-02-06 00:10:21 +0530

    Passed profile variable to template.

--- userprofile/views.py
+++ userprofile/views.py
@@ -7,5 +7,5 @@ from userprofile.models import UserProfile, ProfilePic
 @login_required
 def my_profile(request, template="userprofile/my_profile.html"):
 	profile = get_object_or_404(UserProfile, user=request.user)
-	return render_to_response(template, context_instance=RequestContext(request))
+	return render_to_response(template, { 'profile': profile}, context_instance=RequestContext(request))
 
