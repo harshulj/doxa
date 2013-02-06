5f8a076379402bbf3307ffe00f943dacdca324f9
Harshul Jain <harshulj@gmail.com>
2013-02-06 16:13:12 +0530

    Removed save and delete functions. Changed imagefield to thumbnailerimagefield.

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext as _
 
+from easy_thumbnails.fields import ThumbnailerImageField
 from userprofile.countries import CountryField
+import os
 
 GENDER_CHOICES = ( ('F', _('Female')), ('M', _('Male')),)
 
@@ -25,7 +27,7 @@ class ProfilePic(models.Model):
 	"""
 		Model for a users profile pic.
 	"""
-	image = models.ImageField(upload_to="profilepic/%Y/%b/%d")
+	image = ThumbnailerImageField(upload_to='profilepic/%Y/%b/%d', blank=True)
 	user = models.OneToOneField(User, related_name="profile_pic")
 	valid = models.BooleanField()
 
@@ -34,25 +36,3 @@ class ProfilePic(models.Model):
 
 	def __unicode__(self):
 		return _("%s %s's Profile Pic") % (self.user.first_name , self.user.last_name)
-
-	def delete(self):
-		base, filename = os.path.split(self.image.path)
-		name, extension = os.path.splitext(filename)
-		for key in PROFILEPIC_SIZES:
-			try:
-				os.remove(os.path.join(base, "%s.%s%s" % (name, key, extension)))
-			except:
-				pass
-		super(ProfilePic, self).delete()
-
-	def save(self, *args, **kwargs):
-		for pic in ProfilePic.objects.filter(user=self.user, valid=self.valid).exclude(id=self.id):
-			base, filename = os.path.split(pic.image.path)
-			name, extension = os.path.splitext(filename)
-			for key in PROFILEPIC_SIZES:
-				try:
-					os.remove(os.path.join(base, "%s.%s%s" % (name, key, extension)))
-				except:
-					pass
-			pic.delete()
-		super(ProfilePic, self).save(*args, **kwargs)
