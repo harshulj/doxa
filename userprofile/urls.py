faa7b465ed5fb16da13f4314c84a150025a33d06
Harshul Jain <harshulj@gmail.com>
2013-02-06 19:48:22 +0530

    Added edit profile link to url.

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
-from userprofile.views import my_profile
+from userprofile.views import my_profile, edit_profile
 
 urlpatterns = patterns('',
-	url(r'$', my_profile, name='userprofile_my_profile'),
+	url(r'^$', my_profile, name='userprofile_my_profile'),
+	url(r'^edit/$', edit_profile, name='userprofile_edit_profile'),
 )
