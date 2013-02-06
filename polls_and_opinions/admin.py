a2aa972c291043bd0d631b13873e09695e398b6c
raj <rkssisodiya@gmail.com>
2013-02-06 22:02:27 +0530

    renamed the polls app to polls_and_opinions, added the code for admin site configuration of the models of this app

---
+++ polls_and_opinions/admin.py
@@ -0,0 +1,31 @@
+from models import *
+from django.contrib import admin
+from django.contrib.admin.templatetags.admin_list import date_hierarchy
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 1
+
+class PollAdmin(admin.ModelAdmin):
+    date_hierarchy = 'published_on'
+    search_fields = ['question','author']
+    list_filter = ['author', 'is_live']
+    list_display = ['__unicode__','author','is_live','published_on','duration','last_modified_on']
+    
+    inlines = [ChoiceInline,]
+
+class VoteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    search_fields = ['voter','choice']
+    list_filter = ['voter', 'choice']
+    list_display = ['choice','voter','created_on']
+    
+class OpinionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    search_fields = ['author','text']
+    list_filter = ['author']
+    list_display = ['__unicode__','author','created_on']
+    
+admin.site.register(Poll, PollAdmin)
+admin.site.register(Vote,VoteAdmin)
+admin.site.register(Opinion, OpinionAdmin)
