f6b8771c459f517483e965850f1fa01e89b4e1ba
raj <rkssisodiya@gmail.com>
2013-02-06 19:13:45 +0530

    Gave finishing touches to models. Included the constraint for allowing a user to cast only one vote per poll

--- polls/models.py
+++ polls/models.py
@@ -50,7 +50,7 @@ class Choice(models.Model):
 	author = models.ForeignKey(User)
 	
 	text = models.CharField(max_length = 100)
-	poll = models.ForeignKey(Poll)
+	poll = models.ForeignKey(Poll, related_name="choices")
 	
 	#All choices submitted by users maynot be deemed appropriate by the poll author
 	is_approved = models.BooleanField(verbose_name="Approved",default = False)
@@ -67,10 +67,25 @@ class Vote(models.Model):
 	'''
 	created_on = models.DateTimeField(verbose_name="Time of Creation",auto_now_add=True)
 	voter = models.ForeignKey(User)
-	choice = models.ForeignKey(Choice)
-
-#TODO: A user should be allowed to cast only one vote for a poll
-# If no soln is found, override save and raise an error if integrity violated
+	choice = models.ForeignKey(Choice, related_name="votes")
+	
+	def __unicode__(self):
+		return "%s voted for %s on - %s" % (voter,choice, choice.poll )
+	
+	def validate_unique(self, *args, **kwargs):
+		from django.core.exceptions import ValidationError
+		# A person should be able to cast only one vote per poll.
+		try:
+			votes = Vote.objects.get(choice__poll = self.choice.poll,voter=self.voter)
+		except Vote.DoesNotExist:
+			votes = None
+			
+		if not votes is None:
+			raise ValidationError("A person can cast only one vote per poll. The user %s has already\
+						voted for %s, for the poll %s"%(self.voter.username,self.choice,self.choice.poll))
+		
+		# Call the default validate_unique
+		super(Vote,self).validate_unique(*args, **kwargs)
 
 class Opinion(models.Model):
 	'''
@@ -80,3 +95,4 @@ class Opinion(models.Model):
 	created_on = models.DateTimeField(verbose_name="Time of Creation",auto_now_add=True)
 	author = models.ForeignKey(User)
 	text = models.TextField()
+	
