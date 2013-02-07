21d4cdc625dcf5312ffc2ef21fe6303ee9c0053d
Harshul Jain <harshulj@gmail.com>
2013-02-07 12:41:29 +0530

    Added search, filter, list fields in profile and pic admin.

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from userprofile.models import UserProfile, ProfilePic
 
 class UserProfileAdmin(admin.ModelAdmin):
-	pass
+	search_fields = ['about', 'short_bio', 'user__username', 'user__first_name', 'user__last_name']
+	list_filter = ['dob', 'gender', 'country']
+	list_display = ['__unicode__', 'user', 'dob', 'country', 'gender']
 
 class ProfilePicAdmin(admin.ModelAdmin):
-	pass
+	search_fields = ['user__username', 'user__first_name', 'user__last_name']
+	list_filter = ['valid']
+	list_display = ['image','user', 'valid']
 
 admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(ProfilePic, ProfilePicAdmin)
