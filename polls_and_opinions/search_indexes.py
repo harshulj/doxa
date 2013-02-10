192fc4c1f48df27a8c7442526970821881f1ab50
Harshul Jain <harshulj@gmail.com>
2013-02-10 13:35:21 +0530

    Added Poll index.

--- polls_and_opinions/search_indexes.py
+++ polls_and_opinions/search_indexes.py
@@ -1,14 +1,30 @@
 import datetime
+from django.utils.timezone import now
 from haystack.indexes import *
 from haystack import site
 from polls_and_opinions.models import Opinion, Poll
 
 class OpinionIndex(SearchIndex):
+	"""
+		Search Index for Opinion model.
+	"""
 	text = CharField(document=True, use_template=True)
 	author = CharField(model_attr='author')
 	created_on = DateTimeField(model_attr='created_on')
 	
 	def index_queryset(self):
-		return Opinion.objects.filter(created_on__lte=datetime.datetime.now())
+		return Opinion.objects.filter(created_on__lte=now())
 
+class PollIndex(SearchIndex):
+	"""
+		Search Index for Poll model.
+	"""
+	text = CharField(document=True, use_template=True)
+	author = CharField(model_attr='author')
+	is_live = BooleanField(model_attr='is_live')
+
+	def index_queryset(self):
+		return Poll.objects.filter(created_on__lte=now())
+
+site.register(Poll, PollIndex)
 site.register(Opinion, OpinionIndex)
