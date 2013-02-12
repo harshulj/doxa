16407963310cf9d6f9bfbbce78efa3c1d825f60d
Harshul Jain <harshulj@gmail.com>
2013-02-12 17:12:11 +0530

    Added url to profile views.

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
-from userprofile.views import my_profile, edit_profile
+from userprofile.views import my_profile, edit_profile, user_profile
 
 urlpatterns = patterns('',
-	url(r'^$', my_profile, name='userprofile_my_profile'),
-	url(r'^edit/$', edit_profile, name='userprofile_edit_profile'),
+	url(r'^profile/$', my_profile, name='userprofile_my_profile'),
+	url(r'^profile/edit/$', edit_profile, name='userprofile_edit_profile'),
+	url(r'^user/(?P<username>[\w.@+-]+)/$', user_profile, name='userprofile_user_profile'),
 )
