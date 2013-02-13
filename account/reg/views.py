238e9c024bb262d6f37dceba3ef090a11255863e
Harshul Jain <harshulj@gmail.com>
2013-02-13 14:26:30 +0530

    Added custom login view.

--- account/reg/views.py
+++ account/reg/views.py
@@ -1,6 +1,7 @@
 # Custom register and activate views for django-registration.
 # Used for redirecting user to home if they are already loggedin.
 from django.http import HttpResponseRedirect
+from  django.contrib.auth import views as auth_views
 
 from doxa import settings
 from .forms import UserRegistrationForm
@@ -17,3 +18,9 @@ def register(request, success_url=None,form_class=UserRegistrationForm, profile_
 		return HttpResponseRedirect('/')
 	else:
 		return registration_register(request, backend, success_url, form_class, profile_callback,  extra_context)
+
+def login(request, template='registration/login.html', extra_context=None):
+	if request.user.is_authenticated():
+		return HttpResponseRedirect('/')
+	else:
+		return auth_views.login(request,template)
