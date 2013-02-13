4288c516bcc987c4137ec0fbbe178c661afc36b1
Harshul Jain <harshulj@gmail.com>
2013-02-13 14:10:50 +0530

    Moved register links to account.reg.urls

--- doxa/urls.py
+++ doxa/urls.py
@@ -5,41 +5,17 @@ from doxa import settings
 from django.contrib import admin
 admin.autodiscover()
 
-
-from account.reg.forms import UserRegistrationForm
-from invitation.views import register
-
-# Change URLs given the INVITE_MODE setting, useful for tests
-if getattr(settings, 'INVITE_MODE', False):
-    urlpatterns = patterns('',
-        url(r'^accounts/register/$',    register,
-                                            {
-                                                'form_class': UserRegistrationForm,
-                                                'backend': 'invitation.backends.InvitationBackend',
-                                            },
-                                            name='registration_register'),
-    )
-else:
-    urlpatterns = patterns('',
-        url(r'^accounts/register/$',    register,
-                                            {
-                                                'form_class': UserRegistrationForm,
-                                                'backend': 'account.reg.backends.doxa.DoxaBackend',
-                                            },
-                                            name='registration_register'),
-    )
-
-urlpatterns += patterns('',
+urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/', include('account.reg.backends.doxa.urls')),
-    url(r'^accounts/', include('invitation.urls')),
+    url(r'', include('account.reg.urls')),
+    url(r'^account/', include('invitation.urls')),
     url(r'', include('userprofile.urls')),
     url(r'^polls/', include('polls_and_opinions.urls.polls')),
     url(r'^$', direct_to_template, { 'template': 'index.html'} ),
     url(r'^invite/', include('privatebeta.urls')),
     url(r'^search/', include('haystack.urls')),
-    url(r'accounts/', include('social_auth.urls')),
+    url(r'account/', include('social_auth.urls')),
 )
 
 if settings.DEBUG:
