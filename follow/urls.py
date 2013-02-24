d193df3233244958a90dfa85852bdf72de5041a6
Harshul Jain <harshulj@gmail.com>
2013-02-24 22:55:24 +0530

    Moved relationships template to follow folder and added generic profile template for relationships use. All relationships should extend generic profile.

--- follow/urls.py
+++ follow/urls.py
@@ -5,7 +5,7 @@ from follow.views import relationship_handler
 
 urlpatterns = patterns('',
     url(r'^$', relationship_redirect, name='relationship_list_base'),
-    url(r'^user/(?P<username>[\w.@+-]+)/(?:(?P<status_slug>[\w-]+)/)?$', relationship_list, name='relationship_list'),
-    url(r'^relation/add/(?P<username>[\w.@+-]+)/(?P<status_slug>[\w-]+)/$', relationship_handler, {'add': True}, name='relationship_add'),
-    url(r'^relation/remove/(?P<username>[\w.@+-]+)/(?P<status_slug>[\w-]+)/$', relationship_handler, {'add': False}, name='relationship_remove'),
+    url(r'^user/(?P<username>[\w.@+-]+)/(?:(?P<status_slug>[\w-]+)/)?$', relationship_list, {'template_name': 'follow/relationship_list.html'}, name='relationship_list'),
+    url(r'^relation/add/(?P<username>[\w.@+-]+)/(?P<status_slug>[\w-]+)/$', relationship_handler, {'template_name': 'follow/confirm.html', 'add': True}, name='relationship_add'),
+    url(r'^relation/remove/(?P<username>[\w.@+-]+)/(?P<status_slug>[\w-]+)/$', relationship_handler, {'template_name': 'follow/confirm.html', 'add': False} , name='relationship_remove'),
 )
